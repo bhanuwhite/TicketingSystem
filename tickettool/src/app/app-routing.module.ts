a30ed675cf57ed9dc4ddb92bd1596cd3572a86ad
amitahex <amit.kumar@ahex.co.in>
2023-05-16 10:16:26 +0530

    Added component folder for support & raise-ticket

--- tickettool/src/app/app-routing.module.ts
+++ tickettool/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SupportComponent } from './admin-panel/support/support.component';
+import { RaiseTicketComponent } from './admin-panel/raise-ticket/raise-ticket.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'support', component:SupportComponent},
+  {path:'raise-ticket', component:RaiseTicketComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
