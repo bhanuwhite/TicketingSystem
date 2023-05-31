875222f73c7f1944ede5450b8613bbd0ac9345bf
amitahex <amit.kumar@ahex.co.in>
2023-05-31 16:33:00 +0530

    created components for header,footer,sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminModule } from './components/admin-panel/admin/admin.module';
+import { HeaderComponent } from './common/header/header.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, SidebarComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
