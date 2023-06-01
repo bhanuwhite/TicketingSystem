f1188794db0d0b56503ec23339eb0750d96a786f
Navya Bhukya <navya.bhukya4202@gmail.com>
2023-06-01 19:04:21 +0530

    done with homepage UI

--- src/app/components/admin-panel/admin/admin-routing.module.ts
+++ src/app/components/admin-panel/admin/admin-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SupportComponent } from '../support/support.component';
 import { RaiseTicketComponent } from '../raise-ticket/raise-ticket.component';
+import { FooterComponent } from 'src/app/common/footer/footer.component';
+import { SidebarComponent } from 'src/app/common/sidebar/sidebar.component';
+import { HeaderComponent } from 'src/app/common/header/header.component';
 const routes: Routes = [
   { path: 'support', component: SupportComponent },
   { path: 'raise-ticket', component: RaiseTicketComponent },
+  {path:'footer',component:FooterComponent},
+  {path:'sidebar',component:SidebarComponent},
+  {path:'header',component:HeaderComponent}
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
