bd2559e0488b66343011fc21b23cc55aa39c4849
Navya Bhukya <navya.bhukya4202@gmail.com>
2023-06-12 13:18:46 +0530

    form changes done

--- src/app/common/guard/auth.guard.ts
+++ src/app/common/guard/auth.guard.ts
@@ -20,7 +20,7 @@ export class AuthGuard  {
 
       // not logged in so redirect to login page with the return url
       this.router.navigate(['/login']);
-      return false;
+      return true;
   }
   
 }
