f7c55f3579c693eb3c136dc5c2bfc80497a07507
amitahex <amit.kumar@ahex.co.in>
2023-06-20 18:37:53 +0530

    created UI of invoice page

--- src/app/components/in-voice/in-voice.component.ts
+++ src/app/components/in-voice/in-voice.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-in-voice',
   templateUrl: './in-voice.component.html',
-  styleUrls: ['./in-voice.component.css']
+  styleUrls: ['./in-voice.component.css'],
+  providers: [MessageService, ConfirmationService],
 })
 export class InVoiceComponent {
+  tableData:[]=[];
+  selectedProducts:any
 
+
+
+  editProduct(event:any,id:string){
+
+  }
+  deleteProduct(product:any){
+  }
 }
